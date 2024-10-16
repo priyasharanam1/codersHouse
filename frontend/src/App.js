ef3c7d0d53b42602f919d2fc1fc8c8606ad69e1b
priyasharanam1 <p1910413219@gmail.com>
2024-10-16 19:18:25 +0530

    nav, card components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,21 @@
-import React, {useState} from "react";
-import Expenses from "./components/Expenses/Expenses";
-import "./App.css";
-import NewExpense from "./components/NewExpenses/NewExpense";
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  // Use Routes instead of Switch
+import Home from './pages/Home/Home';
+import Navigation from './components/shared/Navigation/Navigation';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 
-let DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: "School Fee",
-    date: new Date(2023,5,12),
-    amount: 250
-  },
-
-  {
-    id: 'e2',
-    title: "Books",
-    date: new Date(2023,5,12),
-    amount: 100
-  },
-
-  {
-    id: 'e3',
-    title: "House Rent",
-    date: new Date(2023,5,12),
-    amount: 300
-  },
-
-  {
-    id: 'e4',
-    title: "Car Insurance",
-    date: new Date(2023,5,12),
-    amount: 250
-  }
-];
-
-const App = () => {
-
-  const [expenses,setExpenses] = useState(DUMMY_EXPENSES);
-
-  // fetch('http://localhost/sample-api/api/read.php').then(
-  //   response => {
-  //     return response.json();
-  //   }
-  // ).then(
-  //   data =>{
-  //     console.log(data);
-  //     setExpenses(data);
-  //   }
-  // )
-  const addExpenseHandler = (expense) =>{
-    const updatedExpense = [expense, ...expenses];
-    setExpenses(updatedExpense);
-
-  }
-
-  return (
-    <div>
-       <NewExpense onAddExpense = {addExpenseHandler}/>
-       <Expenses item={expenses}/>
-    </div>
-  );
+function App() {
+    return (
+        <BrowserRouter>
+            <Navigation />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
