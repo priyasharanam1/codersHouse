abb5d05de86ab6ba391beb974faafc5386c75b10
priyasharanam1 <p1910413219@gmail.com>
2024-10-21 01:43:24 +0530

    db connection

--- backend/routes.js
+++ backend/routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const authController = require('./controllers/auth-controller.js')
 
 router.post('/api/send-otp', authController.sendOtp);
+router.post('/api/verify-otp', authController.verifyOtp);
 
 module.exports = router;
